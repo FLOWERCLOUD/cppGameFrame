d18db5cb74eb5650700c77a19e1ec735bb505ffd
zhanjunxiong@126.com <zhanjunxiong@126.com@e5d31bda-8885-90e7-4701-246d67164947>
2012-04-14 03:54:16 +0000

    添加广播使用技能的包

---  my-simple-code/src/mysdk/base/Timestamp.cc
+++  my-simple-code/src/mysdk/base/Timestamp.cc
@@ -32,7 +32,8 @@ string Timestamp::toFormattedString() const
 	time_t seconds = static_cast<time_t>(microSecondsSinceEpoch_ / kMicroSecondsPerSecond);
 	int microseconds = static_cast<int>(microSecondsSinceEpoch_ % kMicroSecondsPerSecond);
 	struct tm tm_time;
-	gmtime_r(&seconds, &tm_time);
+	//gmtime_r(&seconds, &tm_time);
+	localtime_r(&seconds, &tm_time);
 
 	snprintf(buf, sizeof(buf) - 1, "%4d-%02d-%02d %02d:%02d:%02d.%06d",
 			tm_time.tm_year + 1900, tm_time.tm_mon + 1, tm_time.tm_mday,
